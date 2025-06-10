5582ee8df1daef7d44cfae56e5402666974ed771
mohamed mohamed <mof4943@gmail.com>
2025-06-10 21:06:36 +0300

    refactor: replace Home class with HomePage widget implementation

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,10 @@
-class Home {
+import 'package:flutter/material.dart';
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar());
+  }
 }
