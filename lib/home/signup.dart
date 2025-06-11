d74830429906598eb588d3a75f8beb837a884512
mohamed mohamed <mof4943@gmail.com>
2025-06-11 21:34:46 +0300

    new signup

--- lib/home/signup.dart
+++ lib/home/signup.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
-    
-class Signup extends StatelessWidget {
 
-  const Signup({ Key? key }) : super(key: key);
-  
+class Signup extends StatelessWidget {
+  const Signup({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      appBar: AppBar(title: const Text('Signup Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Signup Page'),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Go to Home Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Go to Login Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Go to Login Page using Named Route'),
+            ),
+          ],
+        ),
       ),
-      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        child: const Icon(Icons.arrow_back),
+      ),
     );
   }
 }
